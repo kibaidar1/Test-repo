9d3d6e361186d04861d1df55530fcaaea9468ae3
kibaidar1 <aidar1996@gmail.com>
2022-06-26 19:52:43 +0500

    Added checking product name and product price and it works)

---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        button_add_to_basket = self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
+        button_add_to_basket.click()
+        self.solve_quiz_and_get_code()
+
+    def checking_the_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text
+        assert product_name == product_name_in_message, 'There is another PRODUCT NAME in the message'
+
+    def checking_the_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_price_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_MESSAGE).text
+        assert product_price == product_price_in_message, 'There is another PRODUCT PRICE in the cart'
